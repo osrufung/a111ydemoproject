508607c441ac8e837a337871120ba1d91cc892d4
Oswaldo Rubio <oswaldo.rubio@xing.com>
2022-02-18 11:38:41 +0100

    Initial Commit

---
+++ A11yUITestSampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A11yUITestSampleProject
+//
+//  Created by Oswaldo Rubio on 18.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
